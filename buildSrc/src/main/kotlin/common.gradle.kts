6a0ba15076f4f16d84181e501aa0bb175f63c69d
kigawa <mail@kigawa.net>
2025-08-14 20:30:44 +0900

    Yarn.lock ファイルを追加して依存関係を管理

--- buildSrc/src/main/kotlin/common.gradle.kts
+++ buildSrc/src/main/kotlin/common.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 kotlin{
     sourceSets["commonMain"].dependencies {
         implementation("net.kigawa:hakate:3.3.1")
-        implementation("net.kigawa:renlin:1.0.1")
+        implementation("net.kigawa:renlin:1.2.1")
+        implementation("net.kigawa:renlin-router:1.2.0")
     }
 }
